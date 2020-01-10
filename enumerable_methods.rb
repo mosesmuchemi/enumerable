b500512605fa46360e1a597d618db41a62cbf161
Moses Muchemi <moses.muchemi@outlook.com>
2020-01-10 18:07:04 +0300

    impleement and tested the #my_each and #my_each_index enumerable methods

---
+++ enumerable_methods.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Enumerable
+  def my_each
+    return to_enum unless block_given?
+
+    i = 0
+    while i < size
+      yield(self[i])
+      i += 1
+    end
+    self
+  end
+
+  def my_each_with_index
+    return to_enum unless block_given?
+
+    i = 0
+    while i < size
+      yield(self[i], i)
+      i += 1
+    end
+  end
+end
